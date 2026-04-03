58cbd88b6114b305e840309cf56d75587351bdd1
sarocs <samuel.schroeder13@gmail.com>
2026-04-03 11:09:33 -0500

    manual optimization

--- Problem2/sensor-data-processor-main/demo/src/main/java/com/sensordata/SensorDataProcessor.java
+++ Problem2/sensor-data-processor-main/demo/src/main/java/com/sensordata/SensorDataProcessor.java
@@ -27,6 +27,59 @@ public class SensorDataProcessor{
     }
 
     // calculate data
+    // original
+    // public void calculate(double d) {
+
+    //     long startTime = System.nanoTime();
+
+    //     int i, j, k = 0;
+    //     double[][][] data2 = new double[data.length][data[0].length][data[0][0].length];
+
+    //     BufferedWriter out;
+
+    //     // Write racing stats data into a file
+    //     try {
+    //         out = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
+
+    //         for (i = 0; i < data.length; i++) {
+    //             for (j = 0; j < data[0].length; j++) {
+    //                 for (k = 0; k < data[0][0].length; k++) {
+    //                     data2[i][j][k] = data[i][j][k] / d - Math.pow(limit[i][j], 2.0);
+
+    //                     if (average(data2[i][j]) > 10 && average(data2[i][j]) < 50)
+    //                         break;
+    //                     else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
+    //                         break;
+    //                     else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
+    //                             && average(data[i][j]) < data2[i][j][k] && (i + 1) * (j + 1) > 0)
+    //                         data2[i][j][k] *= 2;
+    //                     else
+    //                         continue;
+    //                 }
+    //             }
+    //         }
+
+    //         for (i = 0; i < data2.length; i++) {
+    //             for (j = 0; j < data2[0].length; j++) {
+    //                 out.write(data2[i][j] + "\t");
+    //             }
+    //         }
+
+    //         out.close();
+
+    //         long endTime = System.nanoTime();
+    //         long elapsedMs = (endTime - startTime) / 1_000_000;
+    //         System.out.println("calculate() completed in " + elapsedMs + " ms");
+
+    //     } catch (Exception e) {
+    //         System.out.println("Error= " + e);
+    //         long endTime = System.nanoTime();
+    //         long elapsedMs = (endTime - startTime) / 1_000_000;
+    //         System.out.println("calculate() failed after " + elapsedMs + " ms");
+    //     }
+    // }
+    
+    // manual optimization
     public void calculate(double d) {
 
         long startTime = System.nanoTime();
@@ -42,24 +95,20 @@ public class SensorDataProcessor{
 
             for (i = 0; i < data.length; i++) {
                 for (j = 0; j < data[0].length; j++) {
+                    double currentLimit = limit[i][j];
+                    double currentLimitSquared = currentLimit * currentLimit;
+
                     for (k = 0; k < data[0][0].length; k++) {
-                        data2[i][j][k] = data[i][j][k] / d - Math.pow(limit[i][j], 2.0);
+                        double currentData = data[i][j][k];
+                        data2[i][j][k] = currentData / d - currentLimitSquared;
 
-                        if (average(data2[i][j]) > 10 && average(data2[i][j]) < 50)
+                        double data2Avg = average(data2[i][j]);
+                        double currentData2 = data2[i][j][k];
+                        if (data2Avg > 10 && data2Avg < 50 || currentData2 > currentData)
                             break;
-                        else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
-                            break;
-                        else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
-                                && average(data[i][j]) < data2[i][j][k] && (i + 1) * (j + 1) > 0)
+                        else if (Math.abs(currentData) < Math.abs(currentData2) && average(data[i][j]) < currentData2)
                             data2[i][j][k] *= 2;
-                        else
-                            continue;
                     }
-                }
-            }
-
-            for (i = 0; i < data2.length; i++) {
-                for (j = 0; j < data2[0].length; j++) {
                     out.write(data2[i][j] + "\t");
                 }
             }
@@ -77,5 +126,4 @@ public class SensorDataProcessor{
             System.out.println("calculate() failed after " + elapsedMs + " ms");
         }
     }
-    
 }
